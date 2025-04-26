580a92bf388ffdebaba38a14420fd4d994de3654
mubashir-jaward <mubashirjawardm@gmail.com>
2025-04-26 12:01:03 +0530

    Transaction setAmount Allows Zero Amount

--- src/main/java/cse/school/codejam/Transaction.java
+++ src/main/java/cse/school/codejam/Transaction.java
@@ -24,9 +24,9 @@ public class Transaction {
     }
 
     public Transaction setAmount(double amt) {
-        if (amt == 0) throw new IllegalArgumentException("Amount must be positive.");
-        this.amount = amt; return this;
-    }
+    if (amt == 0) throw new IllegalArgumentException("Amount must be positive.");
+    this.amount = amt; return this;
+}
 
     public String getTransactionDetails() {
         String msg = "";
